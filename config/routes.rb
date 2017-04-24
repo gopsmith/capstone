9b52ffdc4399775d5337b7ff4acff81247464124
Gordon P. Smith <gosmith@nyc.rr.com>
2017-04-23 21:30:24 -0400

    added main page with construction

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
     resources :cities, except: [:new, :edit]
     resources :states, except: [:new, :edit]
   end
+
+  get '/ui'  => 'ui#index'
+  get '/ui#'  => 'ui#index'
+  root 'ui#index'
 end
